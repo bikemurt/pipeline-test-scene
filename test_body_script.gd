a400c8aa645aa50c69f1fedee3b7e1139485d26c
Michael Burt <23486102+bikemurt@users.noreply.github.com>
2025-04-07 21:55:32 -0400

    Initial commit

---
+++ test_body_script.gd
@@ -0,0 +1,4 @@
+extends RigidBody3D
+
+func _ready() -> void:
+	print("Script for a rigid body")
---
+++ test_script.gd
@@ -0,0 +1,5 @@
+extends Node
+
+func _ready() -> void:
+	
+	print("A test script that doesn't do anything")
